a1156ee7b84ae39b0702089043aa32205ce4249e
mahavirsinhpw <mahavirsinhpuwar388@gmail.com>
2025-12-12 11:38:19 +0530

    added another case where missing number ranges from 0 to n

--- src/main/java/com/dsa/arrays/Missing.java
+++ src/main/java/com/dsa/arrays/Missing.java
@@ -5,11 +5,22 @@ package com.dsa.arrays;
 
 public class Missing {
     public static void main(String[] args) {
-
+        // Missing number ranges from 1 to n
         int[] array = {1, 4, 6, 8, 5, 7, 3};
+
+        // Missing number ranges from 0 to n
+//        int [] array = {3,0,1};
+
         int n = array.length;
+
+
         // sum of natural elements
+        // if the number ranges from 1 to n then
         int sumOfNaturalElements = ((n + 1) * (n + 2)) / 2;
+
+        // if the number ranges from 0 to n then
+//        int sumOfNaturalElements = (n*(n+1))/2;
+
         int sum = 0;
         // sum of current elements
         for (int j : array) {
